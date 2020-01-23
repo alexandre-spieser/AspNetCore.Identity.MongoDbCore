6b25924df39fb28e5954198ccb51b769b1ec4deb
Alexandre SPIESER <a.spieser@gmail.com>
2020-01-23 23:07:28 +0000

    Fix errors at startup.

--- test/AspNetCore.Identity.MongoDbCore.IntegrationTests/MockLoggerFactory.cs
+++ test/AspNetCore.Identity.MongoDbCore.IntegrationTests/MockLoggerFactory.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using Microsoft.AspNetCore.Identity.Test;
-using Microsoft.Extensions.Logging;
 
 namespace AspNetCore.Identity.MongoDbCore.IntegrationTests
 {
