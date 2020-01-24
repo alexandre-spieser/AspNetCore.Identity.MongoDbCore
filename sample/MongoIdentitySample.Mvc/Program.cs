59213089eda7e0d4173916ad69e84c88da3c456a
David Barker <david.barker@rise-x.io>
2020-01-24 09:30:34 +0800

    Migrated main to 3.1 format

--- sample/MongoIdentitySample.Mvc/Program.cs
+++ sample/MongoIdentitySample.Mvc/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace MongoIdentitySample.Mvc
 {
@@ -11,15 +12,14 @@ namespace MongoIdentitySample.Mvc
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        protected static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
